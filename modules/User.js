f7c9045aec2949c450da53d50026832f49bf1e76
manish kumar <manishp.dotpitchtechnologies@gmail.com>
2025-12-26 17:03:46 +0530

    Refactor User schema to include new fields

--- modules/User.js
+++ modules/User.js
@@ -1,3 +1,31 @@
+// const mongoose = require("mongoose");
+
+// const userSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     password: {
+//         type: String,
+//         required: true
+//     },
+//     phone: {
+//         type: Number,
+//         required: true,
+//         unique: true
+//     },
+//     createdAt: {
+//         type: Date,
+//         default: Date.now
+//     }
+// });
+
+// module.exports = mongoose.model("User", userSchema);
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
@@ -10,15 +38,38 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-    password: {
-        type: String,
-        required: true
-    },
     phone: {
         type: Number,
         required: true,
         unique: true
     },
+    password: {
+        type: String,
+        required: true
+    },
+
+    // total points
+    totalPoints: {
+        type: Number,
+        default: 100
+    },
+
+    // notification on/off
+    notificationStatus: {
+        type: String,
+        enum: ["on", "off"],
+        default: "on"
+    },
+
+    joinedAt: {
+        type: Date,
+        default: Date.now
+    },
+    lastRewardAt: {
+        type: Date,
+        default: Date.now
+    },
+
     createdAt: {
         type: Date,
         default: Date.now
@@ -26,3 +77,4 @@ const userSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("User", userSchema);
+
