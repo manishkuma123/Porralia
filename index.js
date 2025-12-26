5e8e568d0a1e8033fcf388564b53bd6a0b1b8d0d
manish kumar <manishp.dotpitchtechnologies@gmail.com>
2025-12-26 19:20:03 +0530

    Restore initial Express app setup code
    Uncommented the initial setup code for the Express app.

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// const express = require("express");
-// const connectDB= require('./db')
-// const app = express();
-// require('dotenv').config();
-// const cors = require('cors')
-// const user = require ("./routes/User")
-// app.use(express.json());
-// app.use(cors())
-// connectDB();
-// app.use('/',user)
-// app.get('/',(req,res)=>{
-//     res.send("welcome to Porralia Batting App")
-// })
-
-// console.log('SendGrid API Key:', process.env.SENDGRID_API_KEY ? 'Set' : 'NOT SET');
-// console.log('SendGrid From Email:', process.env.SENDGRID_FROM_EMAIL || 'NOT SET');
-
-// const PORT = process.env.PORT||5000;
-// app.listen(PORT,()=>{
-//     console.log(` server running on port ${PORT}`)
-// })
-
 
 const express = require("express");
 const connectDB= require('./db')
